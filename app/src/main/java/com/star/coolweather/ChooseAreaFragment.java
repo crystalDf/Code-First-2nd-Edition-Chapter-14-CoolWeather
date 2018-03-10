d7cfdb3f212a9ae2c9891a9983f9840bd4e47a51
crystalDf <chendong333@gmail.com>
2018-03-10 23:18:28 +0800

    Chapter 14 CoolWeather

--- app/src/main/java/com/star/coolweather/ChooseAreaFragment.java
+++ app/src/main/java/com/star/coolweather/ChooseAreaFragment.java
@@ -6,12 +6,23 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ListView;
+import android.widget.ProgressBar;
+import android.widget.TextView;
 
 public class ChooseAreaFragment extends Fragment {
 
     private static final int LEVEL_PROVINCE = 0;
     private static final int LEVEL_CITY = 1;
     private static final int LEVEL_COUNTY = 2;
+
+    private ProgressBar mProgressBar;
+    private TextView mTitleText;
+    private Button mBackButton;
+    private ListView mListView;
+    private ArrayAdapter<String> mAdapter;
 
     public ChooseAreaFragment() {
         // Required empty public constructor
