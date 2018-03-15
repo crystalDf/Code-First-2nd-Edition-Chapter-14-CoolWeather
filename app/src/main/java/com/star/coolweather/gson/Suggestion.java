cc937e9f148860f3eb25cc557a2e0df601ce0130
crystalDf <chendong333@gmail.com>
2018-03-15 23:03:51 +0800

    Chapter 14 CoolWeather

---
+++ app/src/main/java/com/star/coolweather/gson/Suggestion.java
@@ -0,0 +1,33 @@
+package com.star.coolweather.gson;
+
+
+import com.google.gson.annotations.SerializedName;
+
+public class Suggestion {
+
+    @SerializedName("comf")
+    private Comfort mComfort;
+
+    @SerializedName("cw")
+    private CarWash mCarWash;
+
+    private Sport mSport;
+
+    private class Comfort {
+
+        @SerializedName("txt")
+        private String mInfo;
+    }
+
+    private class CarWash {
+
+        @SerializedName("txt")
+        private String mInfo;
+    }
+
+    private class Sport {
+
+        @SerializedName("txt")
+        private String mInfo;
+    }
+}
